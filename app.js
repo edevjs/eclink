3abca166d2b66b7ece73b3011955490a2806f95a
Emilio Marin <edevjs@gmail.com>
2021-02-06 20:38:11 +0100

    cors

--- app.js
+++ app.js
@@ -60,6 +60,14 @@ app.use( cors() );
 
 app.use( express.json() );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // Conexión con base de datos
 dbConnection();
 
